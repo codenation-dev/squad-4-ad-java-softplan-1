7a87db2a4ef01f29e21059cb7176639721c236b1
Willian Krueger <wkrueger128@gmail.com>
2019-11-22 20:57:47 -0300

    movendo pastas

--- src/LoginError.java
+++
@@ -1,7 +0,0 @@
-public class LoginError extends Error {
-
-    private String details;
-    /*
-    1) WRONG USER/PASSWORD
-    */
-}
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String args[]) {
-        User testUser = new User("qwerty", "1", "testUser@gmail.com", "Test User");
-        System.out.println(testUser.toString());
-    }
-}
