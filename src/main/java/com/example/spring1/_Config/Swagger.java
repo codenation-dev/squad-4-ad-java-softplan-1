b39c7750fd1dc419625393eeabf119cd5046e1b4
Willian Krueger <wkrueger128@gmail.com>
2019-11-29 13:17:07 -0300

    filtrar rotas no openapi

--- src/main/java/com/example/spring1/_Config/Swagger.java
+++ src/main/java/com/example/spring1/_Config/Swagger.java
@@ -1,10 +1,13 @@
 package com.example.spring1._Config;
 
+import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.ApiListingScannerPlugin;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,9 +16,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger {
 
   @Bean
-  public Docket mainApi() {
-    return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any()).build();
+  public ApiListingScannerPlugin listingScanner() {
+    return new SwaggerManualApiPlugin();
   }
 
+  @Bean
+  public Docket mainApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+      .select()
+      .apis(RequestHandlerSelectors.basePackage("com.example.spring1"))
+      .paths(PathSelectors.any())
+      .build()
+      .apiInfo(apiInfo())
+      .useDefaultResponseMessages(false);
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfo(
+      "Aceleralog",
+      "Serviço coletor de logs.",
+      "1.0.0",
+      null,
+      null,
+      null,
+      null,
+      new ArrayList<>()
+    );
+  }
 }
---
+++ src/main/java/com/example/spring1/_Config/SwaggerManualApiPlugin.java
@@ -0,0 +1,64 @@
+package com.example.spring1._Config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.OperationBuilder;
+import springfox.documentation.service.ApiDescription;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.ApiListingScannerPlugin;
+import springfox.documentation.spi.service.contexts.DocumentationContext;
+import springfox.documentation.spring.web.readers.operation.CachingOperationNameGenerator;
+
+@SuppressWarnings({ "WhitespaceAround", "ParenPad" })
+public class SwaggerManualApiPlugin implements ApiListingScannerPlugin {
+  @Autowired
+  private CachingOperationNameGenerator operationNames;
+
+  @Override
+  public List<ApiDescription> apply(DocumentationContext context) {
+    Set<String> tags = new HashSet<>();
+    tags.add("auth");
+    return new ArrayList<>(
+      Arrays.asList( //<1>
+        new ApiDescription(
+          "getOauthToken",
+          "/oauth/token",
+          "Get oauth token",
+          Collections.singletonList( //<2>
+            new OperationBuilder(operationNames)
+              .authorizations(new ArrayList<>())
+              .codegenMethodNameStem("getOauthToken") //<3>
+              .method(HttpMethod.POST)
+              .tags(tags)
+              // .parameters(
+              //     Collections.singletonList( //<4>
+              //         new ParameterBuilder()
+              //             .description("search by description")
+              //             .type(new TypeResolver().resolve(String.class))
+              //             .name("description")
+              //             .parameterType("query")
+              //             .parameterAccess("access")
+              //             .required(true)
+              //             .modelRef(new ModelRef("string")) //<5>
+              //             .build()))
+              // .responseMessages(responseMessages()) //<6>
+              // .responseModel(new ModelRef("string")) //<7>
+              .build()
+          ),
+          false
+        )
+      )
+    );
+  }
+
+  @Override
+  public boolean supports(DocumentationType delimiter) {
+    return DocumentationType.SWAGGER_2.equals(delimiter);
+  }
+}
